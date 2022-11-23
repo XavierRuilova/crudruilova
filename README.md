6c74b92bdba45101723c85bf3a3655d0581a6699
xavier ruilova accini <xaruilova.dwfsc12@bootcamp.ucamp.io>
2022-11-23 18:52:28 -0500

    botones editar y eliminar creados y funcion eliminar establecida. reorganizacion del codigo. Queda pendiente funcion de modificar daatos y diseno responsivo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # crudruilova
+La pagina muestr aun formulario que permite el resgistro de informacion personal. 
+Al actualizar la pagina buscan registros guardados y se los carga. En caso de no haber informacion para mostrar, el campo queda vacio. 
+En el momento de dar clic en el boton agregar, se verifica si la informacion no esta ya registrada. Caso contrario, se realiza el registro y se actualiza la pagina para poder visualizarlo en el campo. 
+Cada registro aparece con los botones 'editar' y 'eliminar'. en el caso del boton de edicion, se carga el formulario con la informacion del registro correspondiente y el boton agregar cambia al texto modificar. Se modifica el arreglo y se actualiza en la memoria. 
+El boton eliminar suprime directamente el registro de la memoria y del campo de visualizacion. 
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-//SECCION LLAMAR CONTENIDO
+//CAPTURA DE ELEMENTOS DE USO FRECUENTE
+let txtNombre = document.getElementById('inputNombre')
+let txtApellido = document.getElementById('inputApellido')
+let txtEdad = document.getElementById('inputEdad')
+let txtCurso = document.getElementById('inputCurso')
+let txtEmail = document.getElementById('inputMail')
+let txtInfo = document.getElementById('inputInfoAdd')
 let arrayInscripciones = localStorage.getItem('inscripciones')
+let btnAgregar = document.getElementById('btnAgregar')
+
+//SECCION LLAMAR CONTENIDO
 if(arrayInscripciones == null){
     arrayInscripciones = []
 }else{
+    //cargar lo que tengo en la memoria y alamcenarlo en arreglo
     let sectionFichas = document.createElement('section')
     sectionFichas.classList.add('sectionFichas')
     arrayInscripciones = JSON.parse(localStorage.getItem('inscripciones')) 
+    //iteracion de los elementos en el arreglo
     for (let i = 0; i < arrayInscripciones.length; i++){
+        //creacion de elementos con id y clases
         let sectRow = document.createElement('section')
         sectRow.classList.add('rows')
         let artFicha = document.createElement('article')
@@ -17,11 +29,10 @@ if(arrayInscripciones == null){
         btnEdit.classList.add('btnlistDimension')
         btnEdit.classList.add('btnlistEdit')
         btnEdit.setAttribute('id','btnEdit'+i)
-        // btnEdit.setAttribute('value',i)
         let btnDel = document.createElement('div')
         btnDel.classList.add('btnlistDimension')
-        btnDel.setAttribute('id','btnDel'+i)
         btnDel.classList.add('btnlistDel')
+        btnDel.setAttribute('id','btnDel'+i)
         let btnEditTxt = document.createTextNode(`Editar`) 
         let btnDelTxt = document.createTextNode(`Eliminar`) 
         //atributos del objeto
@@ -37,39 +48,58 @@ if(arrayInscripciones == null){
         let Hcurso = document.createElement('h3')
         let Hmail = document.createElement('h3')
         let Hinfo = document.createElement('h3')
-
-        //fin de atributos
         //ensamblaje de elementos
-        document.body.appendChild(sectionFichas)
-        sectionFichas.appendChild(sectRow)
-        Hnombre.appendChild(fichaNombre)
-        Hapellido.appendChild(fichaApellido)
-        Hedad.appendChild(fichaEdad)
-        Hcurso.appendChild(fichaCurso)
-        Hmail.appendChild(fichaMail)
-        Hinfo.appendChild(fichaInfo)
-        sectRow.appendChild(artFicha)
-        artFicha.appendChild(Hnombre)
-        artFicha.appendChild(Hapellido)
-        artFicha.appendChild(Hedad)
-        artFicha.appendChild(Hcurso)
-        artFicha.appendChild(Hmail)
-        artFicha.appendChild(Hinfo)
-        sectRow.appendChild(navBtns)
-        navBtns.appendChild(btnEdit)
-        navBtns.appendChild(btnDel)
-        btnDel.appendChild(btnDelTxt)
-        btnEdit.appendChild(btnEditTxt)
-        //PARA ELIMINAR CONTENIDO
-        let btnDelClick = document.getElementById('btnDel'+i)
+        document.body.appendChild(sectionFichas)//agrego al body la sectionFichas contiene todas las filas de la seccion 
+        sectionFichas.appendChild(sectRow)//agrego la sectionFichas las filas creadas que tienen fondo blanco
+        Hnombre.appendChild(fichaNombre)//agrego al h3 creado la variable que contiene el texto para ese h3
+        Hapellido.appendChild(fichaApellido)//agrego al h3 creado la variable que contiene el texto para ese h3
+        Hedad.appendChild(fichaEdad)//agrego al h3 creado la variable que contiene el texto para ese h3
+        Hcurso.appendChild(fichaCurso)//agrego al h3 creado la variable que contiene el texto para ese h3
+        Hmail.appendChild(fichaMail)//agrego al h3 creado la variable que contiene el texto para ese h3
+        Hinfo.appendChild(fichaInfo)//agrego al h3 creado la variable que contiene el texto para ese h3
+        sectRow.appendChild(artFicha)//Agrego a la franja blanca la seccion que contiene todos los h3
+        artFicha.appendChild(Hnombre)//en la seccion q debe contener todos h3 agrego los h3 que ya tienen su texto
+        artFicha.appendChild(Hapellido)//en la seccion q debe contener todos h3 agrego los h3 que ya tienen su texto
+        artFicha.appendChild(Hedad)//en la seccion q debe contener todos h3 agrego los h3 que ya tienen su texto
+        artFicha.appendChild(Hcurso)//en la seccion q debe contener todos h3 agrego los h3 que ya tienen su texto
+        artFicha.appendChild(Hmail)//en la seccion q debe contener todos h3 agrego los h3 que ya tienen su texto
+        artFicha.appendChild(Hinfo)//en la seccion q debe contener todos h3 agrego los h3 que ya tienen su texto
+        sectRow.appendChild(navBtns)//en la franja blanca arrwgo la secion del nvegador donde van lso botones
+        navBtns.appendChild(btnEdit)//en el navegador donde van los botones agrego los botones que se crearon con div. Caso editar
+        navBtns.appendChild(btnDel)//En el navegador donde van los botones agrego los botones que se crearon con div. caso de eliminar
+        btnDel.appendChild(btnDelTxt)//Agrego el texto al boton eliminar
+        btnEdit.appendChild(btnEditTxt)//Agrego el texto al boton Editar
+        //creacion de accion de boton ELIMINAR
+        let btnDelClick = document.getElementById('btnDel'+i) //btnDel0,btnDel1, btnDel2, btnDel3... 
         btnDelClick.addEventListener('click', (e)=>{
-            idd = btnDelClick.id.replace('btnDel','')
-            // console.log(btnDelClick.id)
-            // console.log(idd)
-            arrayInscripciones.splice(idd,1)
+            let idDel = btnDelClick.id.replace('btnDel','')//i
+            // console.log(btnDelClick.id)//i
+            // console.log(let )
+            arrayInscripciones.splice(idDel ,1)
             // console.log(arrayInscripciones)
+            // console.log(btnDelClick.id)
+            // console.log(let )
             localStorage.setItem('inscripciones', JSON.stringify(arrayInscripciones))
             location.reload()
+            
+        })
+        //creacion de accion de boton EDITAR
+
+        let btnEditClick = document.getElementById('btnEdit'+i)
+        btnEditClick.addEventListener('click', (e)=>{
+            let idEdit= btnEditClick.id.replace('btnEdit','')//i
+            // console.log(btnEditClick.id)
+            // console.log(idEdit)
+            // arrayInscripciones.splice(idEdit,1)
+            // console.log(arrayInscripciones)
+            txtNombre.value = arrayInscripciones[idEdit].nombre
+            txtApellido.value = arrayInscripciones[idEdit].apellido
+            txtEdad.value = arrayInscripciones[idEdit].edad
+            txtCurso.value = arrayInscripciones[idEdit].curso
+            txtEmail.value = arrayInscripciones[idEdit].email
+            txtInfo.value= arrayInscripciones[idEdit].info
+            btnAgregar.setAttribute('style', 'background-color: orange')
+            btnAgregar.innerText='Modificar'
         })
 
     }
@@ -82,27 +112,42 @@ if(arrayInscripciones == null){
 
 
 //SECCION AGREGAR CONTENIDO
-let btnAgregar = document.getElementById('btnAgregar')
 
-btnAgregar.addEventListener('click', (event)=>{
-    let txtNombre = document.getElementById('inputNombre')
-    let txtApellido = document.getElementById('inputApellido')
-    let txtEdad = document.getElementById('inputEdad')
-    let txtCurso = document.getElementById('inputCurso')
-    let txtEmail = document.getElementById('inputMail')
-    let txtInfo = document.getElementById('inputInfoAdd')
-    
-    arrayInscripciones.push(
-        {
-        nombre: txtNombre.value,
-        apellido: txtApellido.value,
-        edad: parseInt(txtEdad.value),
-        curso: txtCurso.value,
-        email: txtEmail.value,
-        info: txtInfo.value, 
+btnAgregar.addEventListener('click', (event)=>{ 
+    c = 0
+    for (let srch = 0; srch < arrayInscripciones.length; srch++){
+        if(arrayInscripciones[srch] === {nombre: txtNombre.value, apellido: txtApellido.value, edad: txtEdad.value, curso: txtCurso.value, email: txtEmail.value, info: txtInfo.value,}){
+            c+=1
+        }
     }
-    )
+    if(c>0){
+        artAlerta = document.createElement('article')
+        artAlerta.classList.add('alerta')
+        artAlertaP = document.createElement('p')
+        artAlertaP.classList.add('alertaP')
+        artAlertaTxt = document.createTextNode(`* Registro existente o sin modificaciones`)
+        document.body.appendChild(artAlerta)
+        artAlerta.appendChild(artAlertaP)
+        artAlertaP.appendChild(artAlertaTxt)
+    }
+    else{
+        arrayInscripciones.push(
+            {
+            nombre: txtNombre.value,
+            apellido: txtApellido.value,
+            edad: parseInt(txtEdad.value),
+            curso: txtCurso.value,
+            email: txtEmail.value,
+            info: txtInfo.value, 
+            }
+        )
+        localStorage.setItem('inscripciones', JSON.stringify(arrayInscripciones))
 
-    localStorage.setItem('inscripciones', JSON.stringify(arrayInscripciones))
+    }
+
+
+        
+
+
 
 })
